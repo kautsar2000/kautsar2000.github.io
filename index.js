ae54acb33f93b6138e6f6de113e529c394819dfb
kautsar2000 <ktsrsyahban@gmail.com>
2023-04-08 11:11:48 +0800

    read

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+import { getDatabase, ref, push,onValue } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBElrUrrKvje2PcoW6TyvCTq5flFIjV_8g",
@@ -68,16 +68,76 @@ simpan.addEventListener("click", function(){
     }
     else {
 
-  push(ref(db, 'siswa'), {
-    nim: nim.value,
-    nama : nama.value,
-    kelas: kelas.value,
-    jenis_kelamin: jenis_kelamin,
-    no_hp: no_hp.value
-  }); }
+        push(ref(db, 'siswa'), {
+            nim: nim.value,
+            nama : nama.value,
+            kelas: kelas.value,
+            jenis_kelamin: jenis_kelamin,
+            no_hp: no_hp.value
+          })
+          .then(() => {
+            // Refresh data setelah berhasil menambahkan
+            swal({
+                title: "Data berhasil ditambahkan!",
+                icon: "success",
+                button: "OK",})
+            $("#staticBackdrop").modal("hide");
+            showDataSiswa();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+ 
+}
 })
 
-let tabel_siswa = document.getElementById('tabel_siswa')
-tabel_siswa.addEventListener('load',function(){
-    console.log('tes')
-})
+// let tabel_siswa = document.getElementById('tabel_siswa')
+// tabel_siswa.addEventListener('load',function(){
+//     console.log('tes')
+// })
+function showDataSiswa() {
+let tabel_siswa = document.getElementById('tabel_siswa');
+onValue(ref(db, 'siswa'), (snapshot) => {
+    let data = snapshot.val();
+    let tabel_siswa_html = '';
+
+    let nomor = 1; // tambahkan variabel nomor
+
+    for (let siswa_id in data) {
+        tabel_siswa_html += '<tr>';
+        tabel_siswa_html += '<td>' + nomor++ + '</td>'; // tambahkan nomor
+        tabel_siswa_html += '<td>' + data[siswa_id].nim + '</td>';
+        tabel_siswa_html += '<td>' + data[siswa_id].nama + '</td>';
+        tabel_siswa_html += '<td>' + data[siswa_id].kelas + '</td>';
+        tabel_siswa_html += '<td>' + data[siswa_id].jenis_kelamin + '</td>';
+        tabel_siswa_html += '<td>' + data[siswa_id].no_hp + '</td>';
+        tabel_siswa_html += '<td>';
+        tabel_siswa_html += '<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modalEditSiswa" onclick="editSiswa(\''+siswa_id+'\')">Edit</button>';
+        tabel_siswa_html += ' <button type="button" class="btn btn-danger btn-sm" onclick="hapusSiswa(\''+siswa_id+'\')">Hapus</button>';
+        tabel_siswa_html += '</td>';
+        tabel_siswa_html += '</tr>';
+    }
+
+    $('#tabel_siswa tbody').html(tabel_siswa_html);
+});}
+//cari siswa
+function cariSiswa() {
+    let input = document.getElementById("cari_siswa").value.toLowerCase();
+    let tabel_siswa = document.getElementById('tabel_siswa');
+    let rowSiswa = tabel_siswa.getElementsByTagName("tr");
+    
+    for (let i = 0; i < rowSiswa.length; i++) {
+      let cell = rowSiswa[i].getElementsByTagName("td")[1];
+      if (cell) {
+        let nama = cell.innerHTML.toLowerCase();
+        if (nama.indexOf(input) > -1) {
+          rowSiswa[i].style.display = "";
+        } else {
+          rowSiswa[i].style.display = "none";
+        }
+      }
+    }
+  }
+
+window.onload = function () {
+    showDataSiswa();}
